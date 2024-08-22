60def0f1b016d4c48b85d177728bc7715de9df5c
Daid <daid303@gmail.com>
2024-08-22 13:08:05 +0200

    Clean IPS code a bit

--- src/ips.py
+++ src/ips.py
@@ -1,4 +1,31 @@
 
+class Patch:
+    def __init__(self, start, size):
+        self.start = start
+        self.size = size
+        self.type = 0
+
+    @property
+    def end(self):
+        return self.start + self.size
+
+
+def findRLESequences(data, min_size=12):
+    rle = []
+    prev = None
+    length = 0
+    for idx, b in enumerate(data):
+        if b == prev:
+            length += 1
+        else:
+            if length >= min_size:
+                rle.append((idx - length, length))
+            prev = b
+            length = 1
+    if length >= min_size:
+        rle.append((len(data) - length, length))
+    return rle
+
 
 # Sloppy IPS patch generator
 def makePatch(old, new, patch):
@@ -12,7 +39,7 @@ def makePatch(old, new, patch):
             while end < len(new) and old[end] != new[end]:
                 end += 1
             size = end - start
-            patches.append((start, size))
+            patches.append(Patch(start, size))
             start = end
         else:
             start += 1
@@ -20,17 +47,17 @@ def makePatch(old, new, patch):
     # Merge patches that are close enough to gether to save record space
     idx = 0
     while idx < len(patches) - 1:
-        if patches[idx][0] + patches[idx][1] >= patches[idx+1][0] - 4 and False:
-            size = (patches[idx+1][0] + patches[idx+1][1]) - patches[idx][0]
-            patches[idx] = (patches[idx][0], size)
+        if patches[idx].end >= patches[idx+1].start - 4:
+            size = patches[idx+1].end - patches[idx].start
+            patches[idx] = Patch(patches[idx].start, size)
             patches.pop(idx+1)
         else:
             idx += 1
 
     patch = open(patch, "wb")
     patch.write(b"PATCH")
-    for start, size in patches:
-        patch.write(bytes([start >> 16, (start >> 8) & 0xFF, start & 0xFF]))
-        patch.write(bytes([size >> 8, size & 0xFF]))
-        patch.write(new[start:start+size])
+    for p in patches:
+        patch.write(bytes([p.start >> 16, (p.start >> 8) & 0xFF, p.start & 0xFF]))
+        patch.write(bytes([p.size >> 8, p.size & 0xFF]))
+        patch.write(new[p.start:p.end])
     patch.write(b"EOF")
